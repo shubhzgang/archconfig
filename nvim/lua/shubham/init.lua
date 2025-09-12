6858c5bce6b73ee183449806a8adcd3d2d7b54f7
Shubham Gangwal <shubhamgang123@gmail.com>
2025-09-12 21:46:44 +0530

    remove nvim config for fresh beginning

--- nvim/lua/shubham/init.lua
+++
@@ -1,8 +0,0 @@
--- Set <Space> as the leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Load core setting, plugins and keymaps
-require('shubham.options')
-require('shubham.packer')
-require('shubham.keymaps')
--- nvim/lua/user/treesitter.lua
+++
@@ -1,7 +0,0 @@
-require('nvim-treesitter.configs').setup({
-  ensure_installed = { 'java', 'lua', 'vim' }, -- Make sure parsers are installed
-  highlight = {
-    enable = true,
-  },
-  indent = { enable = true },
-})
